c8ce8c24feed5c2efba3cc7f6aadb308b4650e76
Drivingh0me <drivingh0me@icloud.com>
2026-09-02 10:18:09 -0500

    Added debug for engine

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod dw_engine;
 pub mod error;
 
 use error::Result;
+use error::Error;
 
 fn main() -> Result<()> {
     let args: cli::Params = cli::get_args()?;
@@ -14,9 +15,8 @@ fn main() -> Result<()> {
 
 fn run_app(args: cli::Params) -> Result<()> {
     let game = dw_engine::GameState::default();
-    // let best_move = dw_engine::BoardState::default();
-    // let eval: f32 = 0;
     let (best_move, eval) = dw_engine::dw_analysis(game, 5)?;
     println!("eval: {}", eval);
+    dbg!(best_move);
     Ok(())
 }
