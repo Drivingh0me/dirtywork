523942346431b14155c425897344ccb7a7154f58
Drivingh0me <drivingh0me@icloud.com>
2026-09-05 10:49:07 -0500

    working on bitboard generator

---
+++ bitboard_generator/main.py
@@ -0,0 +1,14 @@
+import argparse
+
+def main():
+    # Let the user draw a board, and return coresponding u64.
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("-type", help="bitboard type to construct", nargs='1')
+    # args = parser.parse_args()
+    val: int = 0
+
+    print(val)
+
+
+if __name__ == "__main__":
+    main()
