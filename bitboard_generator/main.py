04e1b436896d25b5c0f97150a1adf029d8fa9397
Drivingh0me <drivingh0me@icloud.com>
2026-09-05 15:50:49 -0500

    made bitboard generator

--- bitboard_generator/main.py
+++ bitboard_generator/main.py
@@ -1,11 +1,33 @@
-import argparse
+# import argparse
 
 def main():
     # Let the user draw a board, and return coresponding u64.
     # parser = argparse.ArgumentParser()
     # parser.add_argument("-type", help="bitboard type to construct", nargs='1')
     # args = parser.parse_args()
-    val: int = 0
+    # val: int = 0
+
+    print(" 12345678")
+    h = input("H")
+    g = input("G")
+    f = input("F")
+    e = input("E")
+    d = input("D")
+    c = input("C")
+    b = input("B")
+    a = input("A")
+
+    h = h[::-1]
+    g = g[::-1]
+    f = f[::-1]
+    e = e[::-1]
+    d = d[::-1]
+    c = c[::-1]
+    b = b[::-1]
+    a = a[::-1]
+
+    bin = h+g+f+e+d+c+b+a
+    val = int(bin, 2)
 
     print(val)
 
